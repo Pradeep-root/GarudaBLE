0bfa46f37e1012ee521c60390e6578547881fa29
Pradeep <pradeepemc3@gmail.com>
2022-01-30 18:22:08 +0100

    added interfaces and classes

---
+++ GarudaBle/src/main/java/com/pradeep/garudable/Configuration.java
@@ -0,0 +1,2 @@
+package com.pradeep.garudable;public interface Configuration {
+}
---
+++ GarudaBle/src/main/java/com/pradeep/garudable/GarudaBel.java
@@ -0,0 +1,19 @@
+package com.pradeep.garudable;
+
+public class GarudaBelManager implements GarudaBle {
+
+    @Override
+    public void startBleScan() {
+
+    }
+
+    @Override
+    public void stopBleScan() {
+
+    }
+
+    @Override
+    public void connectBle() {
+
+    }
+}
