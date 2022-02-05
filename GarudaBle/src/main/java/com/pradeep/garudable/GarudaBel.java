7ea8e0df8080a285d721f60baeb50abdfa2625e0
Pradeep <pradeepemc3@gmail.com>
2022-02-05 17:00:23 +0100

    added ble scan method

--- GarudaBle/src/main/java/com/pradeep/garudable/GarudaBel.java
+++ GarudaBle/src/main/java/com/pradeep/garudable/GarudaBel.java
@@ -1,7 +1,33 @@
 package com.pradeep.garudable;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothManager;
+import android.bluetooth.le.BluetoothLeScanner;
+import android.bluetooth.le.ScanCallback;
+import android.bluetooth.le.ScanResult;
+import android.content.Context;
+import android.os.Handler;
+import android.util.Log;
+
+import java.util.List;
+
 public class GarudaBel implements GarudaBle {
 
+    private BluetoothLeScanner bluetoothLeScanner;
+    private boolean scanning;
+    private Handler handler = new Handler();
+
+    // Stops scanning after 10 seconds.
+    private static final long SCAN_PERIOD = 100000;
+
+
+    public GarudaBel(Context context) {
+        final BluetoothManager bluetoothManager =
+                (BluetoothManager) context.getApplicationContext().getSystemService(Context.BLUETOOTH_SERVICE);
+        BluetoothAdapter bluetoothAdapter = bluetoothManager.getAdapter();
+
+        bluetoothLeScanner = bluetoothAdapter.getBluetoothLeScanner();
+    }
 
     @Override
     public void setBleConfiguration() {
@@ -10,16 +36,52 @@ public class GarudaBel implements GarudaBle {
 
     @Override
     public void startBleScan() {
+        if (!scanning) {
+            // Stops scanning after a predefined scan period.
+            handler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    scanning = false;
+                    stopBleScan();
+                }
+            }, SCAN_PERIOD);
 
+            scanning = true;
+            bluetoothLeScanner.startScan(scanCallback);
+        } else {
+            scanning = false;
+            bluetoothLeScanner.stopScan(scanCallback);
+        }
     }
 
     @Override
     public void stopBleScan() {
-
+        bluetoothLeScanner.stopScan(scanCallback);
     }
 
     @Override
     public void connectBle() {
 
     }
+
+    ScanCallback scanCallback = new ScanCallback() {
+        @Override
+        public void onScanResult(int callbackType, ScanResult result) {
+            super.onScanResult(callbackType, result);
+            Log.d("GarudaBle", ""+result.toString());
+        }
+
+        @Override
+        public void onBatchScanResults(List<ScanResult> results) {
+            super.onBatchScanResults(results);
+        }
+
+        @Override
+        public void onScanFailed(int errorCode) {
+            super.onScanFailed(errorCode);
+        }
+    };
+
+
+
 }
--- app/src/main/java/com/pradeep/example/MainActivity.kt
+++ app/src/main/java/com/pradeep/example/MainActivity.kt
@@ -2,11 +2,14 @@ package com.pradeep.example
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.pradeep.garudable.GarudaBel
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val garudaBel = GarudaBel(this)
+        garudaBel.startBleScan()
 
     }
 }
